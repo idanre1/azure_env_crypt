b8d142a465f69623a4f60647545c602dd8302f72
Ubuntu <idanre1@orchestrator.q14jdk54dkcelgkaslmxmd0yrg.bx.internal.cloudapp.net>
2021-09-09 09:09:15 +0300

    hide password

--- env_from_aes_file.sh
+++ env_from_aes_file.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 echo "*** Exporting environment variables"
-eval `python $HOME/azure-env-crypt/env_from_aes_file.py -f $1 -p $2`
+echo "*** Please enter password:   "
+stty -echo
+read password
+stty echo
+eval `python $HOME/azure-env-crypt/env_from_aes_file.py -f $1 -p $password`
 
